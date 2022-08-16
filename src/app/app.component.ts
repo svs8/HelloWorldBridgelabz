346e83f27f59a089a5e45096d3c78f24ac78bc0e
svs8 <surakshithvittalshetty@gmail.com>
2022-08-16 23:09:20 +0530

    Launched Bridgelabz site in a new tab using event binding technique

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'HelloWorldBridgelabz';
   imgUrl="../assets/bridgelabz.png";
+  url = "https://www.bridgelabz.com"
 
 
 ngOnInit(): void {
   this.title="Hello from Bridhelabz";
 }
+
+onClick($event: any){
+  console.log("Save button is clicked!", $event);
+  window.open(this.url, "_blank");
+}
+
 }
